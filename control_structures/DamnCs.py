e2915e2a780d0a38d85da04c0545cb4287d321af
aligholamee <aligholami7596@gmail.com>
2017-09-12 13:34:16 +0430

    Boolean Structures example

---
+++ control_structures/DamnCs.py
@@ -0,0 +1,19 @@
+
+
+# Validating the number's equality
+num = 6
+print('Assignment 1:')
+print(num == 6)
+
+num2 = 10
+print('Assignment 2:')
+print(num < 10)
+
+
+# Boolean operators
+num3 = 25
+str = 'Hello World!'
+if str=='Hello World!' and num==25:
+    print('Success!')
+elif str == 'Hello World!' and num > 2:
+    print('Success!')
