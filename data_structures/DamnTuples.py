f02db30dfa9cbaf70167654c77547c038b6306d5
Ali Gholami <aligholami7596@gmail.com>
2017-09-18 01:03:27 -0700

    Tuple notes were added

---
+++ data_structures/DamnTuples.py
@@ -0,0 +1,15 @@
+# Tuples are used to group any number of items into a single compound value regardless of their type.
+myTuple = (
+    "first",
+    13,
+    "whatever",
+    "lol",
+    12    
+)
+
+print (myTuple[3])
+
+# tuples cannot be reassigned
+# tuple assignment is present however
+# tuples are used for a sequence of values of hetergeneous types: records of csv, database record
+# while lists are used for homogenous types
