688f4ea07b716cd0f5c465e847bd64b278180344
Ali Gholami <aligholami7596@gmail.com>
2017-09-18 00:55:28 -0700

    Extended work with lists

--- data_structures/DamnLists.py
+++ data_structures/DamnLists.py
@@ -10,3 +10,11 @@ print(stack)
 
 # there is no push method here
 # we use append instead
+
+# del statement is used to delete the element from the list but it doesnt return anything
+del stack[0:2]
+print(stack)
+
+# deletes the entire list
+del stack[:]
+print(stack)
