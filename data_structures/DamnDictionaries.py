0080a5273210c1b6e2d3ff79f6b62591db0984a7
Ali Gholami <aligholami7596@gmail.com>
2017-09-17 22:35:02 -0700

    Introduction to dictionaries

---
+++ data_structures/DamnDictionaries.py
@@ -0,0 +1,22 @@
+# The dictionary is Python's main mapping type. It maps hashable values to arbitrary objects as long as they provide the key integrity.
+
+
+# 1st syntax
+d1 = {
+    'first': 1,
+    'second': 2,
+    'third': 3 
+}
+
+# 2nd syntax
+d2 = dict(
+    first = 1,
+    second = 2
+)
+
+
+# now displaying the dictionaries
+print(d1['first'])
+print(d2)
+
+
