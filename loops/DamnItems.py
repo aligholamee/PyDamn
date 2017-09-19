58a13d3547aeb0c8a3116168122fb2103a5050fa
Ali Gholami <aligholami7596@gmail.com>
2017-09-19 01:30:16 -0700

    looping through the items

---
+++ loops/DamnItems.py
@@ -0,0 +1,13 @@
+# this is an example of the dictionary looping
+myDictionary = {
+    'first': 'a',
+    'second': 'b',
+    'third': 'c'
+}
+
+# loop through the items in that dictionary
+for k, v in myDictionary.items():
+    print(k, v)
+
+    
+
