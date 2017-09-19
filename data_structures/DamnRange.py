b6b92b4b1eb0c88301f1a5301a2c8f05a6194e45
Ali Gholami <aligholami7596@gmail.com>
2017-09-19 01:54:46 -0700

    Working with range and loops

---
+++ data_structures/DamnRange.py
@@ -0,0 +1,11 @@
+r = range(1, 5)
+
+# we can have steps in our range
+r2 = range(1, 10, 2)
+print(r2)
+
+# we can combine the range with the for loops to create the customized foor loops
+for i in range(0, 100, 5):
+    print(i)
+
+    
