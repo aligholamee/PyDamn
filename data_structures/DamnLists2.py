53f438f3c389077633f342f35d180295b4d066d9
Ali Gholami <aligholami7596@gmail.com>
2017-09-20 12:11:16 -0700

    Review on the lists

---
+++ data_structures/DamnLists2.py
@@ -0,0 +1,21 @@
+# create a list
+list1 = [1, 2, 3, 5]
+
+# create another list
+list2 = [7, 8, 9, 10]
+
+# extend list1 with the contents of list2
+list1.extend(list2)
+
+# reverse items in list1
+list1.reverse()
+
+# remove a specific item from the list1 
+list1.remove(1)
+
+# remove and return a specific index
+list1.pop([2])
+
+# insert a new item into the list
+# (ide)
+list1.insert(2, 5)
