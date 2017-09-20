8e2fac1da17f6e1c7eaae00977c688e78e6ccd33
Ali Gholami <aligholami7596@gmail.com>
2017-09-20 11:40:10 -0700

    Counting the occurances of a subsequence

--- types/DamnBytearray.py
+++ types/DamnBytearray.py
@@ -3,6 +3,11 @@ a = bytes(b"abc")   # bytes object
 d = a.replace(b"b", b"f") # this will work
 c = a.replace("b", "f")    # this will work
 b = a.replace(b"b", "f")    # this will work
-print(b)
-print(c)
-print(d)
+#print(b)
+#print(c)
+#print(d)
+
+
+# counting the occurances of a subsequence
+testString = bytearray(b"HelloWorld!")
+print(testString.count(b"o"))
