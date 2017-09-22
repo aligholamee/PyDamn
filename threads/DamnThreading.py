fa01622eb4d439db182037f338b36076da221057
Ali Gholami <aligholami7596@gmail.com>
2017-09-22 03:56:53 -0700

    Threads explained

--- threads/DamnThreading.py
+++ threads/DamnThreading.py
@@ -3,7 +3,7 @@ from queue import Queue
 from threading import Thread
 
 def shareQueue(q):
-    while true:
+    while True:
         print(q.get())
         q.task_done()
 
@@ -14,3 +14,11 @@ for i in range(num_threads):
     workerThread = Thread(target=shareQueue, args=(q,))
     workerThread.setDaemon(True)
     workerThread.start()
+
+
+for x in range(50):
+    q.put(x)
+
+# Using the koin method the program will wait until the queue is empty 
+# and the threads are done working(info comes from the task_done method)
+q.join()
