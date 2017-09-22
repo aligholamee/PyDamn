1d88d73c66a5ab90469eedcbfd3e2e05e4abf3ff
Ali Gholami <aligholami7596@gmail.com>
2017-09-22 04:14:44 -0700

    Working with some methods on queues.

---
+++ data_structures/DamnQueueMethods.py
@@ -0,0 +1,13 @@
+import queue
+
+q = queue.Queue(3)
+q.put(1)
+q.put(2)
+q.put(3)
+
+print(q.qsize())
+print(q.empty())
+print(q.full())
+
+# call the block for some time until there is a free slot
+q.put(4, block=True, timeout=500)
