3b54a5febba5af0c462bd71c1ba8246f607edef6
Ali Gholami <aligholami7596@gmail.com>
2017-09-27 17:09:00 +0330

    Datetime module

---
+++ core/DamnDateTime.py
@@ -0,0 +1,10 @@
+# Python's datetimemodule provide as toolki of classes and cuntions for date and time manipulation
+# Objects exposed by this module can be either naive or aware
+# Unlike naive objects, aware objects have enought knowledge to locate themselves relative to other aware objects.
+import datetime
+
+print(datetime.MINYEAR)
+print(datetime.MAXYEAR)
+
+# The datetime module also exports a datetime class
+print(datetime.datetime.today())
